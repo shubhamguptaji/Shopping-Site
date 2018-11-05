a24cbfd174077694734e18238051f4d9c3224cf5
shubhamguptaji <shubham2604gupta@gmail.com>
2018-11-05 23:28:31 +0530

    login page added

--- ShoppingSite/ShoppingSite/settings.py
+++ ShoppingSite/ShoppingSite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ShoppingSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- ShoppingSite/Website/views.py
+++ ShoppingSite/Website/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from .models import Categories, Products, SubCategory
 
-# Create your views here.
-
 def home(request):
     categories = get_list_or_404(Categories)
+    """ find out how many times user visits this page"""
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    print(num_visits)
     return render(request, 'Website/index.html', {'categories' : categories})
 
 def discounts(request):
@@ -21,6 +23,9 @@ def subCategory(request, category):
     return render(request, 'Website/subcategories.html', {'category': category1[0], 'subcategories': subcategories})
 
 def getProduct(request, category, subCategory):
+    category = Categories.objects.filter(category_name=category)
+    subCategory = SubCategory.objects.filter(category__category_name=category.category_name)
+    products = Products.objects.filter(subCategory__subCategory_name=subCategory.subCategory_name)
     return HttpResponse("this is a product of  category: " + category);
 
 def getSpecificProduct(request, category, subCategory, product_id):
